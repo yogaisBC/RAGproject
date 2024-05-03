1a3b542d0c81b476be2fa061ea9cdc4f6e0a1b83
yogaisBC <36303337+yogaisBC@users.noreply.github.com>
2024-05-03 12:19:33 +0100

    feat: additional chunking method

---
+++ te3l_1000char.py
@@ -0,0 +1,22 @@
+import os
+import datetime
+import logging
+import sys
+
+from main import *
+
+now = datetime.datetime.now()
+
+timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+
+filename = f"logs/log_{timestamp}.txt"
+
+logging.basicConfig(filename=filename, level=logging.DEBUG)
+
+load_dotenv()
+
+embed_model = OpenAIEmbedding(model="text-embedding-3-large")
+
+pdf_to_text('law.pdf', 'data/law.txt')
+
+process_queries(query, 'text-embedding-3-large-1000char', '', embed_model)
